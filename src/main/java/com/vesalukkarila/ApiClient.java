6dd7fd389175c278866cad30a58a5155aeb62b99
Vesa Lukkarila <github@vesalukkarila.com>
2024-09-03 12:44:18 +0300

    implement: ApiClient

--- src/main/java/com/vesalukkarila/ApiClient.java
+++ src/main/java/com/vesalukkarila/ApiClient.java
@@ -1,7 +1,24 @@
 package com.vesalukkarila;
 
+import org.apache.hc.client5.http.classic.methods.HttpGet;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
+import org.apache.hc.core5.http.io.entity.EntityUtils;
+import com.google.gson.Gson;
+
+import java.io.IOException;
+
 public class ApiClient {
 
     //using try-with-resources make api call to apiUrl
-    // use gson to parse jsonresponse to User.class
+    public User fetchUserData(String apiUrl) throws IOException {
+        try(CloseableHttpClient httpClient = HttpClientBuilder.create().build()) {
+            return httpClient.execute(new HttpGet(apiUrl), response -> {
+                String jsonResponse = EntityUtils.toString(response.getEntity());
+            // use gson to parse jsonresponse to User.class
+                Gson gson = new Gson();
+                return gson.fromJson(jsonResponse, User.class);
+            });
+        }
+    }
 }
