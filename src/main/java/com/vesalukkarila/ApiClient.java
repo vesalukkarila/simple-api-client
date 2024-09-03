228d0f4e5e6681a424c67b0646e3cee4c642f487
Vesa Lukkarila <github@vesalukkarila.com>
2024-09-03 12:15:54 +0300

    update: todos

---
+++ src/main/java/com/vesalukkarila/ApiClient.java
@@ -0,0 +1,7 @@
+package com.vesalukkarila;
+
+public class ApiClient {
+
+    //using try-with-resources make api call to apiUrl
+    // use gson to parse jsonresponse to User.class
+}
--- src/main/java/com/vesalukkarila/Application.java
+++ src/main/java/com/vesalukkarila/Application.java
@@ -3,5 +3,14 @@ package com.vesalukkarila;
 public class Application {
     public static void main(String[] args) {
         System.out.println("Hello world");
+
+        //Create instance of ApiClient
+        //Set url
+
+        //With try-catch, call client fetchUserData using url
+        //print user´s information
+
+        //In catch-clause, handle exception
+
     }
 }
